df41538c43ba03f845172f1583c568f4b43e5d5b
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-10-01 18:48:42 -0400

    done

--- src/main/java/ex46/App.java
+++ src/main/java/ex46/App.java
@@ -6,20 +6,23 @@ package ex46;
  */
 
 import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 public class App {
+    //Creates a hashamp to send to other functions that uses the word as the key,
+    //will send in the path of where the file is and will call the printing and filtering
+    //functions to make sure the one with the highest count is at the top.
     public static void main(String[] args) throws FileNotFoundException {
         Map<String,Integer> wordCount = new HashMap<>();
         WordFrequency.countFrequency("src/main/java/ex46/exercise46_input.txt",wordCount);
-        printerTime(wordCount);
+        printerTime(wordCount,filterTime(wordCount));
     }
 
-    public static void printerTime(Map<String,Integer> wordCount) {
+    //Filter the hashmap given to us to make sure its ordered by highest count
+    public static Object[] filterTime(Map<String,Integer> wordCount) {
         Object[] filter = wordCount.entrySet().toArray();
         Arrays.sort(filter, new Comparator() {
             public int compare(Object o1, Object o2) {
@@ -27,6 +30,11 @@ public class App {
                         .compareTo(((Map.Entry<String, Integer>) o1).getValue());
             }
         });
+        return filter;
+    }
+
+    //Print everything in a table format and make sure the count is printed in stars
+    public static void printerTime(Map<String,Integer> wordCount,Object[] filter) {
         for(Object e: filter) {
             System.out.format("%-10s: ",((Map.Entry<String, Integer>) e).getKey());
             for(int i = 0;i< (((Map.Entry<String,Integer>) e).getValue()); i++) {
--- src/main/java/ex46/WordFrequency.java
+++ src/main/java/ex46/WordFrequency.java
@@ -6,17 +6,24 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class WordFrequency {
+    //Checks the count of each word and attaches it to the hashmap by going through
+    //each word in the text file
     public static void countFrequency(String fileName, Map<String,Integer> wordCount) throws FileNotFoundException {
         Scanner myInput = new Scanner(new File(fileName));
         while(myInput.hasNext()) {
             String word = myInput.next();
             Integer count = wordCount.get(word);
-            if(count!= null)
-                count++;
-            else
-                count = 1;
+            count = counterCheck(count);
             wordCount.put(word,count);
         }
         myInput.close();
+    }
+
+    //Checks to see if the count is null but otherwise it will just increase by 1
+    //each it sees the word
+    public static Integer counterCheck (Integer count) {
+        if(count!= null)
+            return count+1;
+        return 1;
     }
 }
