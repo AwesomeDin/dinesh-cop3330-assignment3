e80170e205b60292413a65cc350ed9ff69ce3d89
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-10-02 19:22:56 -0400

    done

--- src/main/java/ex45/App.java
+++ src/main/java/ex45/App.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
+    //Gets the output file that the user wants and then passes it into the wordfinder class to create the file
+    //and replace the words.
     public static void main(String[] args) throws IOException {
         String newFile = scanIn("What is the name of the output file? (Please type out the file name completely" +
                 ", example: exercise45_output.txt) ");
@@ -16,6 +18,7 @@ public class App {
                 "use", "src/main/java/ex45/"+ newFile);
     }
 
+    //simple scanin function that gets the userinput for the name of the output file
     public static String scanIn(String s) {
         Scanner myInput = new Scanner(System.in);
         System.out.print(s);
--- src/main/java/ex45/WordFinder.java
+++ src/main/java/ex45/WordFinder.java
@@ -9,20 +9,27 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class WordFinder {
-    static void replaceText(String fileName, String find, String replace,
+    //Replaces the text by finding the word and changing it. Uses Path to modify and write a new file.
+    public static void replaceText(String fileName, String find, String replace,
                             String tofileName) throws IOException {
         Path path = Paths.get(fileName);
         Path toPath = Paths.get(tofileName);
         BufferedWriter writer = Files.newBufferedWriter(toPath, Charset.defaultCharset());
         Scanner myInput = new Scanner(path,Charset.defaultCharset());
+        checkIn(myInput,find,replace,writer);
+        myInput.close();
+        writer.close();
+    }
 
+    //Goes through every word in the textfile to check if there is the target word to replace, once it replaces
+    //it will write that line to the new file.
+    public static void checkIn(Scanner myInput, String find, String replace, BufferedWriter writer) throws IOException {
         while (myInput.hasNextLine()) {
             String line = myInput.nextLine();
             line = line.replaceAll(find,replace);
             writer.write(line);
             writer.newLine();
         }
-        myInput.close();
-        writer.close();
     }
+
 }
